f263ad54a5cf88043aa545db2259b7e5eed518c0
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2019-03-15 09:26:01 +0100

    add accel values

--- app/src/main/java/com/android/diceroll/MainActivity.java
+++ app/src/main/java/com/android/diceroll/MainActivity.java
@@ -1,13 +1,70 @@
 package com.android.diceroll;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sm = null;
+    TextView viewAccel1;
+    TextView viewAccel2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        viewAccel1 = (TextView) findViewById(R.id.textViewAccel1);
+        viewAccel2 = (TextView) findViewById(R.id.textViewAccel2);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensor = event.sensor.getType();
+        float [] values = event.values;
+
+        System.out.println(" " + values.length + " *** " +values[2]);
+
+        synchronized (this) {
+            if (sensor == Sensor.TYPE_ACCELEROMETER) {
+                float x = values[0];
+                float y = values[1];
+                float z = values[2];
+
+                viewAccel1.setText("x " + x);
+                viewAccel2.setText("y " + y + " z " + z);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Sensor mMagneticField = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public int faceRandom() {
+        int max = 6;
+        int min = 1;
+        Random rand = new Random();
+        int nombreAleatoire = rand.nextInt(max - min + 1) + min;
+
+        return nombreAleatoire;
     }
 }
