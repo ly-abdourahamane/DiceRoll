93beb43fa075a0ad56838ec2d0d08e4ed66ab96a
LouisJacques <jacques.louis2981@yahoo.fr>
2019-03-15 13:45:50 +0100

    add menus and activities

--- app/src/main/java/com/android/diceroll/HomeActivity.java
+++ app/src/main/java/com/android/diceroll/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.android.diceroll;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void goToRules(View view){
+        Intent rulesGame = new Intent(getApplicationContext(), RulesActivity.class);
+        startActivity(rulesGame);
+    }
+    public void goToUnlocks(View view){
+        Intent unlockGame = new Intent(getApplicationContext(), AchievementsActivity.class);
+        startActivity(unlockGame);
+    }
+    public void goToPlay(View view){
+        Intent diceGame = new Intent(getApplicationContext(), DiceActivity.class);
+        startActivity(diceGame);
+    }
+    public void goToRScores(View view){
+        Intent scoresGame = new Intent(getApplicationContext(), ScoresActivity.class);
+        startActivity(scoresGame);
     }
 }
